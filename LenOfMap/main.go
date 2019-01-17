cb1e5ecba59f1e2b8f54b1bf4c7ccf4bbb0526f5
ZhangZhitian <dishitian668@gmail.com>
2019-01-17 22:28:14 +0800

    并发安全的map的len方法

---
+++ LenOfMap/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func Len(sm sync.Map) int {
+	lengh := 0
+	f := func(key, value interface{}) bool {
+		lengh++
+		return true
+	}
+	one:=lengh
+	lengh=0
+	sm.Range(f)
+	if one != lengh {
+		one = lengh
+		lengh=0
+		sm.Range(f)
+		if one <lengh {
+			return lengh
+		}
+
+	}
+	return one
+}
+
+func main()  {
+
+	a := sync.Map{}
+
+	a.Store("name","zzt")
+	a.Store("email","dishitin@qq.com")
+	a.Store("age",14)
+	a.Store("age1",15)
+
+	fmt.Println(Len(a))
+
+}
